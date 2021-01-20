ebddae8b0a009c62e8ede7975d93bf7ebc266d72
Michael Cheng <michael_cheng@tech.gov.sg>
2021-01-20 10:04:34 +0800

    Initial commit
    - Add base Gem files

---
+++ lib/formsg/sdk.rb
@@ -0,0 +1,8 @@
+require "formsg/sdk/version"
+
+module Formsg
+  module Sdk
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/formsg/sdk/version.rb
@@ -0,0 +1,5 @@
+module Formsg
+  module Sdk
+    VERSION = "0.1.0"
+  end
+end
